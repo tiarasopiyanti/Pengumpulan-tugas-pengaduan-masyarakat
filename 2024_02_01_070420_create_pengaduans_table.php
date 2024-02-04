9d6c4c4340bb337dba64c601b048cb66e4b4c1b4
tiarasopiyanti <118708571+tiarasopiyanti@users.noreply.github.com>
2024-02-04 07:16:53 -0800

    Add files via upload

---
+++ 2024_02_01_070420_create_pengaduans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pengaduans', function (Blueprint $table) {
+            $table->id('id_pengaduan');
+            $table->date('tanggal_pengaduan');
+            $table->char('nik')->references('nik')->on('masyarakats');
+            $table->text('isi_laporan');
+            $table->string('foto', 255);
+            $table->enum('status', [ 0, 'proses','selesai']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pengaduans');
+    }
+};
