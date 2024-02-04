50d114823fdb1da1cbec40b9a0fa9e7d105eb99e
tiarasopiyanti <118708571+tiarasopiyanti@users.noreply.github.com>
2024-02-04 07:17:12 -0800

    Add files via upload

---
+++ 2024_02_01_070621_create_tanggapans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tanggapans', function (Blueprint $table) {
+            $table->id('id_tanggapan');
+            $table->foreignId('id_pengaduan')->references('id_pengaduan')->on('pengaduans');
+            $table->date('tanggal_tanggapan');
+            $table->text('tanggapan');
+            $table->foreignId('id_petugas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tanggapans');
+    }
+};
